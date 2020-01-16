3a08421583ee9a925753429cbad3df30c34b0ef3
UI Galaxy <uigalaxy7@gmail.com>
2020-01-16 23:16:34 +0600

    fix: remove console

--- src/hocs/withRedBorder.js
+++ src/hocs/withRedBorder.js
@@ -3,7 +3,6 @@ import React from 'react'
 const withRedBorder = Component => {
 
   return  props => {
-     console.log(props);
     return <Component {...props} style={{ border: "1px solid red", fontWeight: "bold", boxShadow: `0 2px 10px rgba(0,0,0,0.5)` }} />;
   }
 }
